06a48deb9a3e5b894c835174e6cb99649601bde0
Akcode01 <arif.kashmiree@gmail.com>
2025-02-04 12:08:05 +0500

    updated

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/About',function(){
+    return "<h2>Just checking a route</h2>";
+});
+
+Route::get('/About/{id}',function($id){
+    return "<h2>Your Route have id:$id</h2>";
+})->whereNumber('id');
+
+Route::get('/Team',function(){
+    return "<h2>Welcome Team page</h2>";
+});
+
+Route::get('/Team/{name}',function($name){
+    return "<h2>Team member: $name </h2>";
+})->whereAlpha('name');
+
+Route::get('/Contact/{num}',function($num){
+    return "<h2>Contact num: $num</h2>";
+})->whereAlphaNumeric('num');
